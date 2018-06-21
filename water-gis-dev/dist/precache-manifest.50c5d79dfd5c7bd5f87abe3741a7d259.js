cbeceb5d9585297142b02e9adae732f31ca560a8
cparadzayi <cparadzayi@gmail.com>
2018-06-21 19:58:02 +0200

    Included nodejs server and built for production

---
+++ water-gis-dev/dist/precache-manifest.50c5d79dfd5c7bd5f87abe3741a7d259.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78913d5af663e1644e1b",
+    "url": "/js/chunk-vendors.78913d5a.js"
+  },
+  {
+    "revision": "46119690b4ba7e92c5b0",
+    "url": "/js/app.46119690.js"
+  },
+  {
+    "revision": "0566733c65e89d878a63efffddec4f64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "46119690b4ba7e92c5b0",
+    "url": "/css/app.31265e30.css"
+  }
+];
---
+++ water-gis-dev/dist/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.50c5d79dfd5c7bd5f87abe3741a7d259.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "water-gis-dev"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
